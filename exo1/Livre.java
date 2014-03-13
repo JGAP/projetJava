3711f60c1d678d111151a5c7a2ddf7113d070da6
JGAP <adeline.blaclard@polytech.univ-montp2.fr>
2014-03-13 16:39:55 +0100

    Test

---
+++ exo1/Livre.java
@@ -0,0 +1,29 @@
+public class Livre {
+	/*******/
+  // Variables
+  private String titre, auteur;
+  private int nbPages;
+
+  // Constructeur
+  public Livre(String unAuteur, String unTitre) {
+    auteur = unAuteur;
+    titre = unTitre;
+  }
+
+  // Accesseur
+  public String getAuteur() {
+    return auteur;
+  }
+
+  // Modificateur
+  void setNbPages(int n) {
+    nbPages = n;
+}
+
+public static void main (String[] args){
+	Livre l1=new Livre("victor hugo", "les miserables");
+	Livre l2=new Livre("zola","germinal");
+	System.out.println("auteur1: " + l1.getAuteur() + "\n auteur2 : "+ l2.getAuteur());
+
+}
+}
